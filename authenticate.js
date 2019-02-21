1ab3dca2a72c163693e8fc53342209d752e8d92e
Armanahmed <aaarmanapu@gmail.com>
2019-02-21 10:36:48 +0600

    HTTPS

--- authenticate.js
+++ authenticate.js
@@ -15,6 +15,16 @@ exports.getToken = function(user) {
 	return jwt.sign(user, config.secretKey, {expiresIn: 3600});
 }
 
+exports.verifyAdmin = function(req, res, next) {
+	if (!req.user.admin) {
+		err = new Error('You are not authorized to perform this operation!');
+		err.status = 403;
+		return next(err);
+	} else {
+		next();
+	}
+}
+
 var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secretKey;
